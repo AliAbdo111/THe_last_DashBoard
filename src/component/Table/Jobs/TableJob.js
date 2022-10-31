b755750dbc85e7d74516d3b69344991f6a8b5112
ALIABDO111 <Aboalhassanabdo769@gmail.com>
2022-10-31 15:56:26 +0200

    the   asdklasjlf

--- src/component/Table/Jobs/TableJob.js
+++ src/component/Table/Jobs/TableJob.js
@@ -1,93 +1,110 @@
 import "../Table.css";
 import axios from "axios";
-import _ from  'lodash'
+import _ from "lodash";
 import React, { useEffect, useState } from "react";
-const pageSize=10;
+const pageSize = 5;
 const baseURL = "http://localhost:7000/Jobs/all";
 function TableJob() {
   const [data, setData] = useState([]);
-  const [serch ,setSearch]=useState('')
-  React.useEffect(() => {
+  const [serch, setSearch] = useState("");
+  const [pagenetdPost, setPage] = useState([]);
+  const [currentPge, setCurrentPge] = useState();
+
+  // const []
+
+  useEffect(() => {
     // invalid url will trigger an 404 error
     axios.get(baseURL).then((response) => {
-      setData(response.data.data);
-   console.log(data)
-    }, []);
-  });
-  const pageCount=data? Math.ceil(data.length/pageSize):0;
-     if(pageCount===1)return null
-     const pages=_.range(1, pageCount+1)
-  function deleteRow( id)
-  {  console.log(id)
+      setData (response.data.data);
+      //  console.log(response.data.data)
+      setPage(_(response.data.data).slice(0).take(pageSize).value());
+    });
+  }, []);
+  console.log(pagenetdPost);
 
-    const jobs = data.filter(item => item._id !== id);  
-      console.log(jobs)
-      setData([...jobs]); 
-    axios.delete(`http://localhost:7000/Jobs/delete/${id}`)  
-    .then(res => {    
-    })  
-    }
+  const pageCount = data ? Math.ceil(data.length/pageSize) : 0;
+  if (pageCount === 1) return null;
+  const pages = _.range(1, pageCount + 1);
+  const pagination = (pagnum) => {
+    console.log(pagnum)
+    setCurrentPge(pagnum);
+    // setData(pagnum)
+    const startIndex = (pagnum - 1) * pageSize;
+    const pagenetdPoste = _(data).slice(startIndex).take(pageSize).value();
+    setPage(pagenetdPoste);
+  };
 
-
+  function deleteRow(id) {
+    console.log(id);
+    const jobs = data.filter((item) => item._id !== id);
+    console.log(jobs);
+  
+    setData([...jobs]);
+    // setData([...jobs]);
+    axios.delete(`http://localhost:7000/Jobs/delete/${id}`).then((res) => {});
+  }
   return (
     <div className="resentOrder">
       <div className="cardHeadr">
         <h2>Jobs Register</h2>
-     <input 
-     type='text'
-      className="search"
-      onChange={(ev)=>setSearch(ev.target.value)}
-      />
+        <input
+          type="text"
+          className="search"
+          onChange={(ev) => setSearch(ev.target.value)}
+        />
       </div>
-      <table>
-        <thead>
-          <tr>
-            <td> Client Name </td>
-            <td> title</td>
-            <td>city</td>
-            <td>address</td>
-            <td>status</td>
-            <td>Delet</td>
-          </tr>
-        </thead>
-        <tbody>
-
-          {data.filter((item)=>item.title.toLowerCase().includes(serch)).map((item) => (
-            <tr key={item._id}>
-                <td>{item.clientId}</td>
-              <td>{item.title}</td>
-              <td>{item.city}</td>
-              <td>{item.address}</td>             
-              <td><p className={
-                item.status?'btn btn-success':'btn btn-danger'
-              } ></p>
-              {item.status?'compelete':'pending'}
-               </td>            
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => {
-                    deleteRow(item._id);
-                  }}
-                >
-                  delete
-                </button>
-              </td>
+      {!data ? (
+        "not found data"
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <td> Client Name </td>
+              <td> title</td>
+              <td>city</td>
+              <td>address</td>
+              <td>status</td>
+              <td>Delet</td>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {
+             pagenetdPost.filter((item) => item.title.toLowerCase().includes(serch))
+              .map((item) => (
+                <tr key={item._id}>
+                  <td>{item.clientId}</td>
+                  <td>{item.title}</td>
+                  <td>{item.city}</td>
+                  <td>{item.address}</td>
+                  <td>{item.status}</td> 
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => {
+                        deleteRow(item._id);
+                      }}
+                    >
+                      delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      )}
       <nav aria-label="Page navigation example">
-  <ul class="pagination">
-    {
-      pages.map((page)=>(
-      <li class="page-item"><a class="page-link" >{page}</a></li>))
-    }
- 
-    
-  </ul>
-</nav>
+        <ul class="pagination">
+          {pages.map((page) => (
+            <li>
+              <p class="page-link" 
+              onClick={() => pagination(page)}>
+                {page}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
-}  
+}
 export default TableJob;
--- src/component/Table/TAbleClient/TAbleClient.js
+++ src/component/Table/TAbleClient/TAbleClient.js
@@ -1,76 +1,73 @@
-import '../Table.css';
+import "../Table.css";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 const baseURL = "http://localhost:7000/Client/all";
 function TAbleClient() {
   const [data, setData] = useState([]);
-  const [serch ,setSearch]=useState('');
+  const [search, setSearch] = useState("");
 
-useEffect(()=>{
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setData(response.data.Data);
+      console.log("jjjjd");
+    });
+  }, []);
 
-  axios.get(baseURL).then((response) => {
-    setData(response.data.Data);
-    console.log("jjjjd")
-  })
-},[])  
-  
+  function deleteRow(id) {
+    console.log(id);
 
-  function deleteRow( id)
-  {  console.log(id)
+    const jobs = data.filter((item) => item._id !== id);
+    console.log(jobs);
 
-    const jobs = data.filter(item => item._id !== id);  
-      console.log(jobs)
-      
-      setData([...jobs]); 
-    axios.delete(`http://localhost:7000/Client/delete/${id}`)  
-    .then(res => {  
-      
-      })  
-    }
+    setData([...jobs]);
+    axios.delete(`http://localhost:7000/Client/delete/${id}`).then((res) => {});
+  }
   return (
     <div className="resentOrder">
       <div className="cardHeadr">
         <h2>Client heloo baby Register</h2>
-        <input 
-     type='text'
-     placeholder="Search About client"
-      className="search"
-      onChange={(ev)=>setSearch(ev.target.value)}
-      />
+        <input
+          type="text"
+          placeholder="Search About client"
+          className="search"
+          onChange={(ev) => setSearch(ev.target.value)}
+        />
       </div>
-            <table>
+      <table>
         <thead>
           <tr>
             <td>Name</td>
             <td>Email</td>
             <td>Adress</td>
-            {/* <td>Status</td> */}
-            {/* <td>skills</td> */}
-            {/* <td>Discration</td> */}
             <td>Phone</td>
-
             <td>gender</td>
-            {/* <td>natinal Id</td> */}
             <td>Delet</td>
           </tr>
         </thead>
         <tbody>
-        {data.filter((item)=>item.firstName.toLowerCase().includes(serch)).map((item) => (
-         
-            <tr key={item._id}>
-              <td>{item.firstName}</td>
-              <td>{item.email}</td>
-              <td>{item.address}</td>
-              {/* <td>{item.status}</td> */}
-              {/* <td>{item.skills[0]}</td> */}
-              {/* <td>{item.description}</td> */}
-              {/* <td>{item.phoneNumber}</td> */}
-              <td>{item.gender}</td>
-              <td>{item.nationalId}</td>
-              <button className="btn btn-danger" onClick={() => deleteRow(item._id)}>Delete</button>
-            </tr>
-          ))}
+          {data
+            .filter(
+              (item) =>
+                item.firstName.toLowerCase().includes(search) ||
+                item.address.toLowerCase().includes(search)
+            )
+            .map((item) => (
+              <tr key={item._id}>
+                <td>{item.firstName}</td>
+                <td>{item.email}</td>
+                <td>{item.address}</td>
+
+                <td>{item.gender}</td>
+                <td>{item.nationalId}</td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteRow(item._id)}
+                >
+                  Delete
+                </button>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
