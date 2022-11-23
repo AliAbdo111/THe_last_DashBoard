03f411bce1c0472716dfbc97d342445d60814725
ALIABDO111 <Aboalhassanabdo769@gmail.com>
2022-11-23 21:56:21 +0200

    add charts

--- src/Pages/Charts.js
+++ src/Pages/Charts.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 import 'reactochart/styles.css';
 import colors from './../../node_modules/d3-scale-chromatic/src/categorical/Accent';
 import { set } from 'lodash';
-const baseURL = "http://localhost:7000/Jobs/all";
+const baseURL = "http://localhost:7000/Jobs/dates";
 
         const Charts = (props) => {
 
@@ -17,60 +17,53 @@ const baseURL = "http://localhost:7000/Jobs/all";
           useEffect(() => {
             axios.get(baseURL).then((response) => {
               // console.log(response.data.data);
-              setData(response.data.data)
-              // console.log(pagenetdPost);
+
+              setData(response.data.Data)
+              console.log(response.data.Data);
             });
 
 
           }, []);
-          Data.map((item)=>{
-            const dd= item.hiredDate
-            const arrOfDateJob=[]
-            arrOfDateJob.push(dd)
-            
-            console.log(arrOfDateJob.length)
 
-
-          })
-          const countData=Data.length
-          // console.log(countData)
+         
+         
+   
 
 
 
 
 //y is count job
 //x is the day create jobs
-            const data = [
-                {dd:1 ,y:countData ,z:0},
-              // {x: 1, y: 2, z: 0},
-              // {x: 5, y: 22, z: 12},
-              // {x: 6, y: 26, z: 13},
-              // {x: 7, y: 26, z: 14},
-              // {x: 10, y: 32, z: 0},
-              // {x: 30, y: 10, z: 0},
-              // {x:30 ,y:30 ,z:0},
-            ];
-            return <XYPlot width={1000} height={600} colors={'#9B0000'}>
-              <XAxis showGrid={false} title="Days since Jobs" />
+            // const data = [
+                
+            //   {x: 1, y: 2, z: 0},
+            //   {x: 2, y: 10, z: 12},
+            //   {x: 4, y: 15, z: 13},
+            //   {x: 6, y: 15, z: 13},
+            //   {x: 8, y: 17, z: 14},
+            //   {x: 10, y: 20, z: 15},
+            //   {x: 12, y: 23, z: 16},
+            //   {x: 14, y: 25, z: 16},
+            //   {x: 16, y: 27, z: 16},
+            //   {x: 18, y: 30, z: 16},
+            //   {x:20 ,y:34 ,z:17},
+            // ];
+            return <XYPlot width={1000} height={600} >
+              <XAxis showGrid={true} title="Days since Jobs" />
               <YAxis title="Count"/>
               <BarChart
-                data={data}
-                x={d => d.x}
-                y={d => d.y}
+                data={Data}
+                x={d => d.date}
+                y={d => d.totalJobs}
                 barThickness={10}
-                barStyle={{fill:"#9B0000"}}
+                barStyle={{fill:"#001253"}}
               />
+          
               <LineChart
-                data={data}
-                x={d => d.x}
-                y={d => d.y}
-                lineStyle={{stroke: '#000B49', strokeWidth: 5}}
-              />
-              <LineChart
-                data={data}
-                x={d => d.x}
-                y={d => d.z}
-                lineStyle={{stroke: '#9B0000', strokeWidth: 3}}
+                data={Data}
+                x={d => d.date}
+                y={d => d.totalJobs}
+                lineStyle={{stroke: '#FFDE00', strokeWidth: 3}}
               />
             </XYPlot>
           };
